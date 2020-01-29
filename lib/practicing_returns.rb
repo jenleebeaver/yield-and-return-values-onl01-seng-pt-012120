78da9be7fc7ee46cb29b71285b405b206c285cb5
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-01-29 07:05:28 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -4,11 +4,12 @@ def hello(array)
   i = 0
   collection = []
   while i < array.length
-    collection << yield(array[i])
-    binding.pry
-    yield(array[i])
+    collection << yield(array[i#])
+    #binding.pry <- old code 
+    #yield(array[i])
     i += 1
   end
+  collection
 end
 
 
